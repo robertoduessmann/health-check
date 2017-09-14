c6f55d127b53cf871806a38417927589fd1da761
Roberto Duessmann <roberto.duessmann@gmail.com>
2017-09-14 20:30:58 -0300

    Added health and memory models

---
+++ model/health.go
@@ -0,0 +1,5 @@
+package model
+
+type Health struct {
+	Memory Memory `json:"memory"`
+}
---
+++ model/memory.go
@@ -0,0 +1,7 @@
+package model
+
+type Memory struct {
+	Total string `json:"total"`
+	Used  string `json:"used"`
+	Free  string `json:"free"`
+}
