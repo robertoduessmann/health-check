a17f40e53da1aebdd28dd6540d9525d64d54a1d2
Alex Dzyoba <alex@dzyoba.com>
2017-10-24 10:42:04 +0300

    Working disk checks

--- controller/disk.go
+++ controller/disk.go
@@ -2,7 +2,9 @@ package controller
 
 import (
 	"encoding/json"
+	"errors"
 	"log"
+	"strings"
 
 	"github.com/robertoduessmann/health-check/model"
 )
@@ -31,8 +33,15 @@ func HardDiskCheck() (hardDisks []model.HardDisk) {
 	diskNames := getDiskNames(devices)
 
 	for _, diskName := range diskNames {
-		// Get "df /dev/diskName" output
-		log.Println(diskName)
+		dfCommand := ExecCommand("df -B1 /dev/" + diskName)
+		dfResult := dfCommand.String()
+		diskInfo, err := parseDf(dfResult)
+		if err != nil {
+			log.Printf("df parse error for /dev/%s: %q", diskName, err)
+			continue
+		}
+		diskInfo.Name = diskName
+		hardDisks = append(hardDisks, diskInfo)
 	}
 
 	return
@@ -58,5 +67,19 @@ func getDiskNames(devices Devices) (diskNames []string) {
 
 func parseLsblk(cmdOutput string) (devices Devices, err error) {
 	err = json.Unmarshal([]byte(cmdOutput), &devices)
+	return
+}
+
+func parseDf(cmdOutput string) (disk model.HardDisk, err error) {
+	lines := strings.Split(cmdOutput, "\n")
+	if len(lines) != 3 { // 3 is empty newline at the end
+		return disk, errors.New("Invalid df output")
+	}
+
+	fields := strings.Fields(lines[1])
+	disk.Total = fields[1]
+	disk.Used = fields[2]
+	disk.Free = fields[3]
+
 	return
 }
